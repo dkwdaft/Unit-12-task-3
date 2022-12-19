d3d85f6fe66e5e8934ec14e06257d3a354ac1825
dkwdaft <31158132+dkwdaft@users.noreply.github.com>
2022-12-19 13:43:05 +0000

    Add files via upload

---
+++ Hello World.py
@@ -0,0 +1,11 @@
+from guizero import App, TextBox, PushButton, Text
+
+def update_text():
+    label.value = name.value
+
+app = App()
+label = Text(app, text="What's your name?")
+name = TextBox(app)
+button = PushButton(app, command=update_text)
+
+app.display(
