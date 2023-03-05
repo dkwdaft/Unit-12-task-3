f149c276fec8685770358e78571f6355da12ef15
dkwdaft <31158132+dkwdaft@users.noreply.github.com>
2023-03-05 12:13:20 +0000

    add placeholder, for starting amount and exchange rate

--- main.py
+++ main.py
@@ -7,29 +7,29 @@ app = App(title="Currency Converter")
 # Create main box for containing layout constraints
 master_box = Box(app, layout="grid")
 # Create parent box object for containing all objects relating to entering the starting currency
-starting_currency_box = Box(master_box, border=border_width, grid=[0, 0])
+starting_currency_box = Box(master_box, grid=[0, 0])
 # Create label for the starting currency, drop-down box
 starting_currency_label = Text(starting_currency_box, text="Starting Currency:", )
 # Create a drop-down box for entering the starting currency
 starting_currency_dropdown = Combo(starting_currency_box, options=options)
 # Create parent box object for containing all objects relating to entering the starting amount
-starting_amount_box = Box(master_box, border=border_width, grid=[1, 0])
+starting_amount_box = Box(master_box, grid=[1, 0])
 # Create label for the starting amount text entry box
 starting_amount_label = Text(starting_amount_box, text="Starting Amount:")
 # Create text entry box for entering the starting amount
-starting_amount_text_entry_box = TextBox(starting_amount_box)
+starting_amount_text_entry_box = TextBox(starting_amount_box, text="0.00")
 # Create parent box object for containing all objects relating to entering the desired currency
-desired_currency_box = Box(master_box, border=border_width, grid=[2, 0])
+desired_currency_box = Box(master_box, grid=[2, 0])
 # Create label for the desired currency, drop-down box
 desired_currency_label = Text(desired_currency_box, text="Desired Currency:")
 # Create a drop-down box for entering the desired currency
 desired_currency_dropdown = Combo(desired_currency_box, options=options)
 # Create parent box object for containing all objects relating to entering the exchange rate
-exchange_rate_box = Box(master_box, border=border_width, grid=[1, 1])
+exchange_rate_box = Box(master_box, grid=[1, 1])
 # Create label for the starting amount text entry box
 exchange_rate_label = Text(exchange_rate_box, text="Exchange Rate:")
 # Create text entry box for entering the starting amount
-exchange_rate_text_entry_box = TextBox(exchange_rate_box)
+exchange_rate_text_entry_box = TextBox(exchange_rate_box, text="0.00")
 # Create box for holding the results label
 # results_box = Box(master_box, grid=[1,2])
 # Create text label for displaying the results of the calculation
